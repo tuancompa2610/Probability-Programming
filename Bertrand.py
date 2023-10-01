a18e724cd7db5c8e945920ae1c89ece1c05d16c4
tuancompa2610 <tuanb2610@gmail.com>
2023-10-01 12:24:08 +0200

    Signed-off-by: tuancompa2610 <tuanb2610@gmail.com>

--- Bertrand.py
+++ Bertrand.py
@@ -11,20 +11,51 @@ import sys
 
 # Model 1
 # n is the number of experiments
+def dis(x_1, y_1, x_2, y_2):
+    return math.sqrt((x_1 - x_2)**2 + (y_1 - y_2)**2)
+
+
 def model1(n):
     # jn is a list of the x coordinates of the steps, rel is list of the values of the cdf
+    rel = [0] + [i / n for i in range(n+1)]
+    jn = []
+    for _ in range(n):
+        r = random.uniform(-1, 1)
+        jn.append(2 * math.sqrt(1 - r**2))
+    jn.sort()
+    jn = [0] + jn + [2]
     return [jn, rel]
 
 
 # Model 2
 def model2(n):
     # jn is a list of the x coordinates of the steps, rel is list of the values of the cdf
+    rel = [0] + [i / n for i in range(n + 1)]
+    jn = []
+    x_1, y_1 = 1, 0
+    for _ in range(n):
+        alpha = random.uniform(0, math.pi)
+        x_2, y_2 = math.cos(alpha), math.sin(alpha)
+        jn.append(dis(x_1, y_1, x_2, y_2))
+    jn.sort()
+    jn = [0] + jn + [2]
     return [jn, rel]
 
 
 # Model 3
 def model3(n):
     # jn is a list of the x coordinates of the steps, rel is list of the values of the cdf
+    rel = [0] + [i / n for i in range(n + 1)]
+    jn = []
+    for _ in range(n):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        while dis(x, y, 0, 0) > 1:
+            x = random.uniform(-1, 1)
+            y = random.uniform(-1, 1)
+        jn.append(2 * math.sqrt(1 - dis(x, y, 0, 0)**2))
+    jn.sort()
+    jn = [0] + jn + [2]
     return [jn, rel]
 
 
--- CDF.py
+++ CDF.py
@@ -3,17 +3,57 @@ I encoded this program myself, did not copy or rewrite the code of others,
 and did not give or send it to anyone else.
 Do Chau Tuan
 """
-import scipy
-
+import math
+import random
+import scipy.integrate as integrate
+from fractions import Fraction
 
 def F(x):
 
     if 0 <= x <= 1.5:
         return 2 * x**2 / 9
     else:
-        -x**2 + 4*x - 3
+        return -x**2 + 4*x - 3
+
+
+def G(x):
+    # G = 1 - F for calculating the expected value
+    if 0 <= x <= 1.5:
+        return 1 - 2 * x**2 / 9
+    else:
+        return 1 - (-x**2 + 4*x - 3)
+
+
+def monte_carlo_simulation():
+    sample = []
+    for _ in range(1000):
+        u = random.uniform(0, 1)
+        while 0.5 < u <= 0.75:
+            u = random.uniform(0, 1)
+        if u <= 0.5:
+            x = math.sqrt(4.5 * u)
+        else:
+            x = -math.sqrt(1 - u) + 2
+        sample.append(round(x, 3))
+    return sample
+
+
+
+def expect_value():
+    return integrate.quad(G, 0, 2, )[0]
 
 
 def main():
+    X = monte_carlo_simulation()
+    freq = 0
+    for i in range(len(X)):
+        if X[i] == 1.500:
+            freq += 1
+    print(f"{sum(X) / len(X):.5f}")
+    print(f"{freq / len(X):.3f}")
+    print(f"{expect_value():.5f}")
+    print(Fraction(integrate.quad(F, 0, 1.5)[0]))
+
 
 if __name__ == "__main__":
+    main()
